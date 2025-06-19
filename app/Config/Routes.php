c89e60212b097b335c2d2ab7e6d8b7dd3489280b
vendo-code <doniduwi43@gmail.com>
2025-06-19 23:26:58 +0700

    menambahkan data transaksi pembelian

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,6 +35,12 @@ $routes->group('ProdukCategory', ['filter' => 'auth'], function ($routes) {
     $routes->get('delete/(:any)', 'ProdukCategoryController::delete/$1');
 });
 
+$routes->get('checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
+$routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
+
+$routes->get('get-location', 'TransaksiController::getLocation', ['filter' => 'auth']);
+$routes->get('get-cost', 'TransaksiController::getCost', ['filter' => 'auth']);
+
 $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
 
 $routes->get('contact', 'ContactController::index', ['filter' => 'auth']);

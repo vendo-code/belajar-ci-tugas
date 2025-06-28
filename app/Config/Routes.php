c42363682176c3d7345076a7e61f60f96738e81c
vendo-code <doniduwi43@gmail.com>
2025-06-28 13:31:09 +0700

    menambahkan api data transaksi pembelian

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -41,6 +41,10 @@ $routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
 $routes->get('get-location', 'TransaksiController::getLocation', ['filter' => 'auth']);
 $routes->get('get-cost', 'TransaksiController::getCost', ['filter' => 'auth']);
 
+$routes->get('profile', 'Home::profile', ['filter' => 'auth']);
+
 $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
 
 $routes->get('contact', 'ContactController::index', ['filter' => 'auth']);
+
+$routes->resource('api', ['controller' => 'apiController']);
--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -28,6 +28,14 @@
         <?php
         }
         ?>
+
+        <li class="nav-item">
+    <a class="nav-link <?php echo (uri_string() == 'profile') ? "" : "collapsed" ?>" href="profile">
+        <i class="bi bi-person"></i>
+        <span>Profile</span>
+    </a>
+</li><!-- End Profile Nav -->
+
           <li class="nav-item">
                 <a class="nav-link <?php echo (uri_string() == 'Contact') ? "" : "collapsed" ?>" href="contact">
                     <i class="bi bi-envelope"></i>
